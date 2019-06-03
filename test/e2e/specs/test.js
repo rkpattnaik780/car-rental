478c2230dda54d3ae9e9fadb08b114f87e703612
rkpattnaik780 <rkpattnaik780@gmail.com>
2019-06-03 18:15:23 +0530

    test: write a simple night watch test
    A very basic nightwatch test has been written.

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -11,9 +11,8 @@ module.exports = {
     browser
       .url(devServer)
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
+      .assert.containsText('div.headline', 'Book cars at touch of your palm')
+      .assert.elementCount('button', 1)
       .end()
   }
 }
