88d9bcac4e70c3e1e53e0ab96886379b76e97b0d
rkpattnaik780 <rkpattnaik780@gmail.com>
2019-06-03 17:34:43 +0530

    style: perform formatting for readability
    Files have been formatted to improve readability and remove eslint errors.

--- src/components/CarCard.vue
+++ src/components/CarCard.vue
@@ -1,40 +1,53 @@
 <template>
-   <v-card min-width="275px" max-width="550px" hover :dark="isSelected">
-       <v-layout row wrap>
-        <v-flex>
-        <v-img
-          class="red--text"
-          :src="photo"
-          width="270px"
-          height="162px"
-        >
-        <v-container fluid v-if="!isAvailable">
+  <v-card min-width="275px" max-width="550px" hover :dark="isSelected">
+    <v-layout row wrap>
+      <v-flex>
+        <v-img class="red--text" :src="photo" width="270px" height="162px">
+          <v-container fluid v-if="!isAvailable">
             <v-layout>
               <v-flex xs12 align-start flexbox>
                 <span class="headline">Not Available</span>
               </v-flex>
             </v-layout>
           </v-container>
-          </v-img>
-          </v-flex>
-          <v-flex>
-          <v-card-text class="text-xs-left">
-            <div style="width:200px;">
-              <h3>{{ name }}</h3>
-              <div><i class="fas fa-fw fa-dollar-sign ma-1"></i>{{ price }}</div>
-              <div><i class="fas fa-fw fa-car ma-1"></i>{{ carType }}</div>
-              <div><i class="fas fa-fw fa-gas-pump ma-1"></i>{{ fuelType }}</div>
-              <div><i class="fas fa-fw fa-wrench ma-1"></i>{{ transmission }}</div>
-              <div><i class="fas fa-fw fa-map-marker-alt ma-1"></i>{{ location }}</div>
-              <div><i class="fas fa-fw fa-users ma-1"></i>{{ seats }}</div>
+        </v-img>
+      </v-flex>
+      <v-flex>
+        <v-card-text class="text-xs-left">
+          <div style="width:200px;">
+            <h3>{{ name }}</h3>
+            <div>
+              <i class="fas fa-fw fa-dollar-sign ma-1"></i>
+              {{ price }}
             </div>
-          </v-card-text>
-          </v-flex>
-       </v-layout>
-       <v-layout xs12 px-2>
-           <v-btn block color="success" :disabled="!isAvailable" align-center @click="select">SELECT</v-btn>
-       </v-layout>
-        </v-card> 
+            <div>
+              <i class="fas fa-fw fa-car ma-1"></i>
+              {{ carType }}
+            </div>
+            <div>
+              <i class="fas fa-fw fa-gas-pump ma-1"></i>
+              {{ fuelType }}
+            </div>
+            <div>
+              <i class="fas fa-fw fa-wrench ma-1"></i>
+              {{ transmission }}
+            </div>
+            <div>
+              <i class="fas fa-fw fa-map-marker-alt ma-1"></i>
+              {{ location }}
+            </div>
+            <div>
+              <i class="fas fa-fw fa-users ma-1"></i>
+              {{ seats }}
+            </div>
+          </div>
+        </v-card-text>
+      </v-flex>
+    </v-layout>
+    <v-layout xs12 px-2>
+      <v-btn block color="success" :disabled="!isAvailable" align-center @click="select">SELECT</v-btn>
+    </v-layout>
+  </v-card>
 </template>
 
 <script>
@@ -42,13 +55,12 @@ export default {
   name: 'CarCard',
   data () {
     return {
-      isSelected : false
+      isSelected: false
     }
   },
   methods: {
-    select(){
+    select () {
       this.isSelected = !this.isSelected
-      console.log("select triggered")
     }
   },
   props: {
@@ -58,14 +70,9 @@ export default {
     location: String,
     seats: Number,
     isAvailable: Boolean,
-    fuelType : String,
+    fuelType: String,
     transmission: String,
     carType: String
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,101 +1,82 @@
 <template>
   <v-container fluid fill-height>
     <v-layout align-center justify-center>
-    <v-flex xs12 sm5>
-      <v-card>
-        <v-card-title>
-          <v-layout my-2 align-center justify-center row>
-            <v-flex xs12>
-          <div class="headline">Book cars at touch of your palm</div>
-            </v-flex>
-          </v-layout>
-          <v-form>
-    <v-layout row wrap mx-3>
-      <v-flex xs12 grow>
-          <v-layout  align-center justify-center row>
-            <v-flex xs12>
-            <v-select
-              fluid
-              :items="locations"
-              v-model="selectedLocation"
-              label="Location"
-            ></v-select>
-          </v-flex>
-          </v-layout>
-      </v-flex>
-      <v-flex xs12>
-        <v-menu
-          lazy
-          :close-on-content-click="true"
-          v-model="menu"
-          transition="scale-transition"
-          offset-y
-          full-width
-          :nudge-right="40"
-          max-width="290px"
-          min-width="290px"
-        >
-          <v-text-field
-            slot="activator"
-            label="Date"
-            v-model="date"
-            prepend-icon="event"
-            readonly
-          ></v-text-field>
-          <v-date-picker v-model="date" required no-title scrollable actions>
-          </v-date-picker>
-        </v-menu>
-      </v-flex>
-      <v-flex xs12 align-center justify-center>
-        <v-btn block rounded color="success"  @click="submit">Submit</v-btn>
+      <v-flex xs12 sm5>
+        <v-card>
+          <v-card-title>
+            <v-layout my-2 align-center justify-center row>
+              <v-flex xs12>
+                <div class="headline">Book cars at touch of your palm</div>
+              </v-flex>
+            </v-layout>
+            <v-form>
+              <v-layout row wrap mx-3>
+                <v-flex xs12 grow>
+                  <v-layout align-center justify-center row>
+                    <v-flex xs12>
+                      <v-select
+                        fluid
+                        :items="locations"
+                        v-model="selectedLocation"
+                        label="Location"
+                      ></v-select>
+                    </v-flex>
+                  </v-layout>
+                </v-flex>
+                <v-flex xs12>
+                  <v-menu
+                    lazy
+                    :close-on-content-click="true"
+                    v-model="menu"
+                    transition="scale-transition"
+                    offset-y
+                    full-width
+                    :nudge-right="40"
+                    max-width="290px"
+                    min-width="290px"
+                  >
+                    <v-text-field
+                      slot="activator"
+                      label="Date"
+                      v-model="date"
+                      prepend-icon="event"
+                      readonly
+                    ></v-text-field>
+                    <v-date-picker v-model="date" required no-title scrollable actions></v-date-picker>
+                  </v-menu>
+                </v-flex>
+                <v-flex xs12 align-center justify-center>
+                  <v-btn block rounded color="success" @click="submit">Submit</v-btn>
+                </v-flex>
+              </v-layout>
+            </v-form>
+          </v-card-title>
+        </v-card>
       </v-flex>
     </v-layout>
-    </v-form>
-        </v-card-title>
-      </v-card>
-    </v-flex>
-  </v-layout>
-    
   </v-container>
 </template>
 
 <script>
 export default {
   name: 'LandingPage',
-  methods:{
-    submit(){
-      console.log(this.selectedLocation)
-      console.log(this.date)
-      this.$router.push({ path: 'cars', query: { locality : this.selectedLocation , date : this.date} })
+  methods: {
+    submit () {
+      this.$router.push({
+        path: 'cars',
+        query: { locality: this.selectedLocation, date: this.date }
+      })
       this.$router.go()
     }
   },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      locations: ["Koramangala","HSR Layout","Indiranagar"],
-      selectedLocation : "Koramangala",
-      menu : false,
+      locations: ['Koramangala', 'HSR Layout', 'Indiranagar'],
+      selectedLocation: 'Koramangala',
+      menu: false,
       date: new Date().toISOString().substr(0, 10)
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
